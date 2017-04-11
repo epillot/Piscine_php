2660ddf6da711c6a191dc081565b629e6b2881e5
Pillot Etienne <pillotetienne@MacBook-Air-de-Pillot.local>
2017-04-12 00:21:11 +0200

    d06/ex02

--- d06/ex02/Vector.class.php
+++ d06/ex02/Vector.class.php
@@ -1,39 +1,31 @@
 <?php
 
+require_once 'Color.class.php';
+
 Class Vector {
 
 	private $_x;
 	private $_y;
 	private $_z;
-	private $_w;
+	private $_w = 0.0;
 	public static $verbose = false;
 
 	public function getX() { return $this->_x; }
 	public function getY() { return $this->_y; }
 	public function getZ() { return $this->_z; }
 	public function getW() { return $this->_w; }
-	public function getColor() { return $this->_color; }
-
-	public function setX($val) { $this->_x = $val; return; }
-	public function setY($val) { $this->_y = $val; return; }
-	public function setZ($val) { $this->_z = $val; return; }
-	public function setW($val) { $this->_w = $val; return; }
-	public function setColor(Color $c) { $this->_color = $c; return; }
 
 	public function __construct(array $kwargs) {
-		if (array_key_exists('x', $kwargs) != false && array_key_exists('y', $kwargs) != false && array_key_exists('z', $kwargs) != false)
+		if (array_key_exists('dest', $kwargs) != false)
 		{
-			$this->_x = $kwargs['x'];
-			$this->_y = $kwargs['y'];
-			$this->_z = $kwargs['z'];
-			if (array_key_exists('w', $kwargs) != false)
-				$this->_w = $kwargs['w'];
+			$dest = $kwargs['dest'];
+			if (array_key_exists('orig', $kwargs) != false)
+				$orig = $kwargs['orig'];
 			else
-				$this->_w = 1.0;
-			if (array_key_exists('color', $kwargs) != false)
-				$this->_color = $kwargs['color'];
-			else
-				$this->_color = new Color( array('rgb' => 0xffffff) );
+				$orig = new Vertex( array( 'x' => 0, 'y' => 0, 'z' => 0) );
+			$this->_x = $dest->getX() - $orig->getX();
+			$this->_y = $dest->getY() - $orig->getY();
+			$this->_z = $dest->getZ() - $orig->getZ();
 		}
 		else
 			print( 'Invalid paramaters' . PHP_EOL );
@@ -49,16 +41,57 @@ Class Vector {
 	}
 
 	public static function doc() {
-		return PHP_EOL . file_get_contents('Vertex.doc.txt');
+		return PHP_EOL . file_get_contents('Vector.doc.txt');
 	}
 
 	public function __toString() {
-		if (self::$verbose == false)
-			return sprintf('Vertex( x: %.2f, y: %.2f, z:%.2f, w:%.2f )', $this->_x, $this->_y, $this->_z, $this->_w);
-		else
-			return sprintf('Vertex( x: %.2f, y: %.2f, z:%.2f, w:%.2f, %s )', $this->_x, $this->_y, $this->_z, $this->_w, $this->_color);
+		return sprintf('Vector( x: %.2f, y: %.2f, z:%.2f, w:%.2f )', $this->_x, $this->_y, $this->_z, $this->_w);
 	}
 
+	public function magnitude() {
+		return sqrt($this->_x * $this->_x + $this->_y * $this->_y + $this->_z * $this->_z);
+	}
+
+	public function normalize() {
+		$dest = new Vertex( array( 'x' => $this->_x / $this->magnitude(), 'y' => $this->_y / $this->magnitude(), 'z' => $this->_z / $this->magnitude() ) );
+		return new Vector( array( 'dest' => $dest ) );
+	}
+
+	public function add(Vector $rhs) {
+		$dest = new Vertex( array( 'x' => $this->_x + $rhs->x, 'y' => $this->_y + $rhs->y, 'z' => $this->_z + $rhs->z ) );
+		return new Vector( array( 'dest' => $dest ) );
+	}
+
+	public function sub(Vector $rhs) {
+		$dest = new Vertex( array( 'x' => $this->_x - $rhs->x, 'y' => $this->_y - $rhs->y, 'z' => $this->_z - $rhs->z ) );
+		return new Vector( array( 'dest' => $dest ) );
+        }
+
+	public function opposite() {
+		$dest = new Vertex( array( 'x' => -$this->_x, 'y' => -$this->_y, 'z' => -$this->_z ) );
+		return new Vector( array( 'dest' => $dest ) );
+        }
+	
+	public function scalarProduct($k) {
+		$dest = new Vertex( array( 'x' => -$this->_x * $k, 'y' => -$this->_y * $k, 'z' => -$this->_z * $k ) );
+		return new Vector( array( 'dest' => $dest ) );
+        }
+
+	public function dotProduct(Vector $rhs) {
+		return $this->_x * $rhs->_x + $this->_y * $rhs->_y + $this->_z * $rhs->_z;
+	}
+
+	public function cos(Vector $rhs) {
+                return $this->dotProduct($rhs) / ($this->magnitude() * $rhs->magnitude());
+        }
+
+	public function crossProduct(Vector $rhs) {
+		$x = $this->_y * $rhs->_z - $this->_z * $rhs->_y;
+		$y = $this->_z * $rhs->_x - $this->_x * $rhs->_z;
+		$x = $this->_x * $rhs->_y - $this->_y * $rhs->_x;
+		$dest = new Vertex( array( 'x' => $x, 'y' => $y, 'z' => $z ) );
+		return new Vector( array( 'dest' => $dest ) );
+        }
 }
 
 ?>
